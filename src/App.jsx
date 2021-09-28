59703ad422ce19b46aa181845f4a78e91b341090
Juan Fernando <juanfed12345@gmail.com>
2021-09-28 14:13:12 -0500

    Modifique el modo de trabajar las rutas mediante el archivo jsconfig,json, creacion de la carpeta pages para el enlace entre paginas y styles para los estilos, ademas de modularizar algunas cosas

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+// importe de stilos de la pagina
+
+import 'styles/resect.css';
+
+// dependencias 
+
+import Index from 'pages';
+
+
+function App() {
+  return ( /*Aca se incluiran todas las paginas de nuestro sitio web */
+    <div className="App">
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+// esta sera el index de la pagina, es decir (Lo que se muestra por defecto al ingresar a la pagina)
+function Index (){
+    return(
+        <div>
+            <h1>Aca sera hará la parte del login, con su correspondiente formulario</h1>
+        </div>
+    )
+}
+
+export default Index;
