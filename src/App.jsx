d51eedac8cde7d28d983074e447af4ee287aaa7b
Juan Fernando <juanfed12345@gmail.com>
2021-09-28 20:21:49 -0500

    Creacion del componente Header y el componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import 'styles/resect.css';
 // dependencias 
 
 import Index from 'pages';
+import Footer from 'components/Footer';
 
 
 function App() {
   return ( /*Aca se incluiran todas las paginas de nuestro sitio web */
     <div className="App">
       <Index/>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div>
+            <h1>Aca ira el footer</h1>
+        </div>
+    )
+}
+
+export default Footer;
