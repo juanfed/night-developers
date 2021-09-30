f4f260d1bd085c4a77e20cb94f5a170ddd07f71e
Camilo <velasquezcristian923@gmail.com>
2021-09-30 00:30:16 -0500

    pruebas de cambio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import 'styles/header.css'
 function Header(){
     return(
-        <div>
+        <div className="header">
             <h4>Acá ira el menu de navegacion que se motrara a lo largo de toda pagina independientemente de donde me encuentre, junto con el nombre de la empresa y el boton lateral dereco de salir.</h4>
         </div>
     )
