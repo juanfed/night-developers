0a1bf8d2eddee333da317fe7d35399d5a75f7e4b
Juan Fernando <juanfed12345@gmail.com>
2021-10-02 17:44:19 -0500

    Integracion del menu de navegacion que realizo Cristian Camilo, a la pagina del proyecto de misiontic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,64 @@
-import 'styles/header.css'
+import 'styles/header.css';
+import Transicion from 'components/script';
+import {useEffect} from 'react';
+
+
 function Header(){
+    useEffect(()=>{
+        Transicion()
+    },[])
     return(
         <div className="header">
-            <h4>Acá ira el menu de navegacion que se motrara a lo largo de toda pagina independientemente de donde me encuentre, junto con el nombre de la empresa y el boton lateral dereco de salir.</h4>
+            <div id="body-pd">
+    <header className="header" id="header">
+        
+        <div className="header__toggle">
+        <i className='bx bx-menu' id="header-toggle"></i>
+        </div>
+        <div>
+        <h1>PC - Master Race</h1>
+        </div>
+        <div className="header__img">
+        <img src="img/jx6WiEM.jpg" alt="" />
+        </div>
+    </header>
+
+    <div className="l-navbar" id="nav-bar">
+        <nav className="nav">
+
+        <div>
+            <a href="#" className="nav__logo">
+            <i className='bx bx-layer' nav__logo-icon></i>
+            <span className="nav__logo-name">MasterRace</span>
+            </a>
+
+            <div className="nav__list">
+            <a href="#" className="nav__link active">
+                <i className='bx bxs-user nav__icon'></i>
+                <span className="nav__name">Usuario</span>
+            </a>
+
+            <a href="#" className="nav__link">
+                <i className='bx bxs-cart-alt nav__icon'></i>
+                <span className="nav__name">Productos</span>
+            </a>
+
+            <a href="#" className="nav__link">
+                <i className='bx bxs-bar-chart-alt-2 nav__icon'></i>
+                <span className="nav__name">Ventas</span>
+            </a>
+
+            </div>
+
+        </div>
+        <a href="#" className="nav__link">
+            <i className='bx bx-log-out nav__icon'></i>
+            <span className="nav__name">Salir</span>
+        </a>
+
+        </nav>
+    </div>
+</div>
         </div>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,9 +6,7 @@ function Index (){
         <div>
             <Header />
             <div>
-                <h1 className="index">esta sera el index de la pagina, despues de ingresar al sistema ya sea como usuario o registrandose, aca ira un contenido donde 
-                    se ponga todo lo relacionado con la pagina como nombre de la empresa, fotos, y demas cosas.
-            </h1>
+                
             </div>
             
         </div>
