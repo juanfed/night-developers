eae776ccb6fdb2c67a84c88f6a0b656d7801f33f
juanfed <juanfed12345@gmail.com>
2021-12-22 16:49:28 -0500

    mejoré la pagina para cuando el usuarrio ingrese a rutas sin permisos, le agregué estilos y una imagen de error 404

--- src/components/RutaPrivada.jsx
+++ src/components/RutaPrivada.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from 'react-router-dom';
+import '../styles/rutaPrivada.css';
+
+import pageNoFound from '../imagenes/pageNoFound.png';
 
 const RutaPrivada = ({ children }) => {
     const { isAuthenticated, isLoading } = useAuth0();
 
     if (isLoading) return <div> Loding..</div>;
-    return isAuthenticated ? <>{children}</> : (<div>
-        <div>Uste no esta autorizado a acceder a este sitio</div>
-        <div>organizar la pagina y hacer que aparesca un boton para devolverme al home o un boton para que 
-            el usuario que no este registrado, se pueda registrar o iniciar secion
+    return isAuthenticated ? <>{children}</> : (
+    <div className='autenticacion'>
+        <div className='autenticacion__tittle'>
+            <h1>Usted no esta autorizado para acceder a la ruta</h1>
         </div>
-    </div>)
+        <div>
+            <figure>
+                <img src={pageNoFound} alt="" />
+            </figure>
+        </div>
+        <div className='autenticacion__link'>
+            <Link to="/"><p>Volver a la pagina de inicio</p></Link>
+        </div>
+        
+    </div>
+    )
 }
 
 export default RutaPrivada
